ccaa8f4ef60e7fd75311356e4848739eaee8257f
V Saravanan <saravanan.resume@gmail.com>
2018-05-22 01:22:20 +0800

    blankit

---
+++ redux-counter/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const element = (
+<div>
+</div>
+)
+
+export default class App extends React.Component {
+  render() {
+    return ( element) ;
+  }
+}
---
+++ redux-counter/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// Import the App container component
+import App from './App'
+
+// Pass the store into the app container
+render(<App />,  document.getElementById('root'));
