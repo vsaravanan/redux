e4b6fa7ae2265aa32ec0d37b2220b1a3ecca107c
V Saravanan <saravanan.resume@gmail.com>
2018-05-21 21:51:07 +0800

    addToCart

--- redux-first/src/index.js
+++ redux-first/src/index.js
@@ -1,5 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 
+const productsReducer = function(state=[], action) {
+  return state;
+}
+
 const initialState = {
     cart: [
       {
@@ -15,12 +19,32 @@ const initialState = {
     ]
   }
 
-const cartReducer = function(state=initialState, action) {
-  return state;
+  const ADD_TO_CART = 'ADD_TO_CART';
+
+  const cartReducer = function(state=initialState, action) {
+    switch (action.type) {
+      case ADD_TO_CART: {
+        return {
+          ...state,
+          cart: [...state.cart, action.payload]
+        }
+      }
+  
+      default:
+        return state;
+    }
+  }
+
+function addToCart(product, quantity, unitCost) {
+    return {
+        type: ADD_TO_CART,
+        payload: { product, quantity, unitCost }
+    }
 }
 
 const allReducers = {
-  shoppingCart: cartReducer
+    products: productsReducer,
+    shoppingCart: cartReducer
 }
 
 const rootReducer = combineReducers(allReducers);
@@ -29,3 +53,13 @@ let store = createStore(rootReducer);
 
 console.log("initial state: ", store.getState());
 
+
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
+
+store.dispatch(addToCart('Coffee 500gm', 1, 250));
+store.dispatch(addToCart('Flour 1kg', 2, 110));
+store.dispatch(addToCart('Juice 2L', 1, 250));
+
+unsubscribe();
