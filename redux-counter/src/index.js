390c953089684009f0692b65d4d1754c6f064311
V Saravanan <saravanan.resume@gmail.com>
2018-05-22 01:31:36 +0800

    click on body

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -13,7 +13,10 @@ const counter = (state = 0, action) => {
 
 const store = createStore(counter);
 
-console.log(store.getState());
+store.subscribe(() => {
+    document.body.innerText = store.getState();
+});
 
-store.dispatch({ type: 'INCREMENT'});
-console.log(store.getState());
+document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT'});
+})
