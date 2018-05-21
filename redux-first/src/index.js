cfb307f068a037db757ccb394420d818bdef7328
V Saravanan <saravanan.resume@gmail.com>
2018-05-21 21:39:56 +0800

    cart 2 items

--- redux-first/src/index.js
+++ redux-first/src/index.js
@@ -1,15 +1,25 @@
 import { createStore, combineReducers } from 'redux';
 
-const productsReducer = function(state=[], action) {
-  return state;
-}
+const initialState = {
+    cart: [
+      {
+        product: 'bread 700g',
+        quantity: 2,
+        unitCost: 90
+      },
+      {
+        product: 'milk 500ml',
+        quantity: 1,
+        unitCost: 47
+      }
+    ]
+  }
 
-const cartReducer = function(state=[], action) {
+const cartReducer = function(state=initialState, action) {
   return state;
 }
 
 const allReducers = {
-  products: productsReducer,
   shoppingCart: cartReducer
 }
 
