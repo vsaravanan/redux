c15e556866e951db21c6fead68b057ff0dc56cd3
V Saravanan <saravanan.resume@gmail.com>
2018-05-22 11:12:33 +0800

    toggleTodo

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -116,10 +116,34 @@ const testIncrementCounter = () => {
 
 }
 
+
 testAddCounter();
 testRemoveCounter();
 testIncrementCounter();
 console.log('All tests are passed');
+
+
+const toggleTodo = (todo) => {
+
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  }
+  const todoAfter  = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  }
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+}
+
+testToggleTodo(); // run the test
 
 store.subscribe(render);
 render();
