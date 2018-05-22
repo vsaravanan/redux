53d09f174b666174679c8d55b331d9ce7d446678
V Saravanan <saravanan.resume@gmail.com>
2018-05-22 11:32:23 +0800

    writing a todo list reducer

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -139,26 +139,7 @@ const toggleTodo = (todo) => {
     };
 };
 
-const testToggleTodo = () => {
-  const todoBefore = {
-    id: 0,
-    text: 'Learn Redux',
-    completed: false
-  }
-  const todoAfter  = {
-    id: 0,
-    text: 'Learn Redux',
-    completed: true
-  }
 
-  deepFreeze(todoBefore);
-
-  expect(
-    toggleTodo(todoBefore)
-  ).toEqual(todoAfter);
-}
-
-testToggleTodo(); // run the test
 
 store.subscribe(render);
 render();
@@ -204,5 +185,47 @@ const testAddTodo = () => {
 };
 
 testAddTodo();
+
+
+
+const testToggleTodo = () => {
+    const stateBefore = [
+    {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+    ];
+    const action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    };
+    const stateAfter = [
+    {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+    ];
+  
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+  
+    expect(
+      todos(stateBefore, action)
+    ).toEqual(stateAfter);
+  }
+
+testToggleTodo(); // run the test
 
 console.log('All tests are passed');
