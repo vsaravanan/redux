2b438705ad8971f7f9fabf17b3ef6c5d1b585021
V Saravanan <saravanan.resume@gmail.com>
2018-05-22 11:00:11 +0800

    slice and spread

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -57,9 +57,14 @@ const addCounter = (list) => {
 const removeCounter = (list, index) => {
     //list.splice(index, 1);
     //return list;
-    return list
-    .slice(0, index)
-    .concat(list.slice(index + 1));
+    // return list
+    // .slice(0, index)
+    // .concat(list.slice(index + 1));
+
+    return [
+        ...list.slice(0,index),
+        ...list.slice(index+1)
+    ];
 };
 
 const testAddCounter = () => {
