0ea4160eb05111dc5522ba44637a425fc09c5e93
V Saravanan <saravanan.resume@gmail.com>
2018-05-22 10:56:18 +0800

    using splice at removeCounter

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -53,6 +53,11 @@ const addCounter = (list) => {
     return [...list, 0];
 };
 
+const removeCounter = (list, index) => {
+    list.splice(index, 1);
+    return list;
+};
+
 const testAddCounter = () => {
     const listBefore = []; 
     const listAfter = [0];
@@ -64,7 +69,18 @@ const testAddCounter = () => {
     ).toEqual(listAfter);
 }
 
+
+const testRemoveCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 20];
+
+    expect (
+        removeCounter(listBefore, 1)
+    ).toEqual(listAfter);
+}
+
 testAddCounter();
+testRemoveCounter();
 console.log('All tests are passed');
 
 store.subscribe(render);
