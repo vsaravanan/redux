8a80e291cac5a2515a95adbcedf5e1bcfce0020a
V Saravanan <saravanan.resume@gmail.com>
2018-05-22 18:01:13 +0800

    removed unwanted unit tests

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,144 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux' ;
-import expect from 'expect';
-import deepFreeze from 'deep-freeze';
+//import expect from 'expect';
+//import deepFreeze from 'deep-freeze';
 //import { composeWithDevTools } from 'redux-devtools-extension';
-
-const counter = (state = 0, action) => {
-  switch (action.type) {
-  case 'INCREMENT':
-    return state + 1;
-  case 'DECREMENT':
-    return state - 1;
-  default:
-    return state;
-  }
-}
-
-const Counter = ({
-    value,
-    onIncrement,
-    onDecrement
-    }) => (
-        <div>
-            <h1> {value} </h1>
-            <button onClick={onIncrement}> + </button>
-            <button onClick={onDecrement}> - </button>
-        </div>
-);
-
-
-
-const renderOld = () => {
-    ReactDOM.render (
-        <Counter 
-        value = {store.getState()} 
-        onIncrement={ () =>
-            store.dispatch({
-                type:'INCREMENT'
-            })
-        }
-        onDecrement={ () =>
-            store.dispatch({
-                type:'DECREMENT'
-            })
-        }        
-        />,
-        document.getElementById('root')
-    );
-};
-
-const addCounter = (list) => {
-    //list.push(0);
-    return [...list, 0];
-};
-
-const removeCounter = (list, index) => {
-    //list.splice(index, 1);
-    //return list;
-    // return list
-    // .slice(0, index)
-    // .concat(list.slice(index + 1));
-
-    return [
-        ...list.slice(0,index),
-        ...list.slice(index+1)
-    ];
-};
-
-const testAddCounter = () => {
-    const listBefore = []; 
-    const listAfter = [0];
-
-    deepFreeze(listBefore);
-
-    expect (
-        addCounter(listBefore)
-    ).toEqual(listAfter);
-}
-
-
-const testRemoveCounter = () => {
-    const listBefore = [0, 10, 20];
-    const listAfter = [0, 20];
-
-    deepFreeze(listBefore);
-
-    expect (
-        removeCounter(listBefore, 1)
-    ).toEqual(listAfter);
-}
-
-const incrementCounter = (list, index) => {
-    // return list
-    //   .slice(0, index)
-    //   .concat(list[index] + 1)
-    //   .concat(list.slice(index + 1));
-    return [
-        ...list.slice(0, index),
-        list[index] + 1,
-        ...list.slice(index + 1)
-      ];
-
-   };
-
-// write a test/assertion before implementing the function:
-const testIncrementCounter = () => {
-  const listBefore = [0, 10, 20];
-  const listAfter  = [0, 11, 20];
-
-  deepFreeze(listBefore);
-  
-  expect(
-    incrementCounter(listBefore, 1)
-  ).toEqual(listAfter);
-
-}
-
-
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
-
-
-
-const toggleTodo = (todo) => {
-    // todo.completed = !todo.completed;
-    // return todo;
-
-    // ES6
-    // return Object.assign({}, todo, {
-    //     completed: !todo.completed
-    //   });  
-    
-    // ES7
-    return {
-        ...todo,
-        completed: !todo.completed
-    };
-};
-
 
 
 
@@ -165,11 +30,7 @@ const todo = (state, action) => {
 const todos = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
-        //   return [
-        //     ...state,
-        //     todo(undefined, action)
-        //   ];
-        // array spread causing errors...
+
 
         var newState = state.map( (e) => { return e; }); // copy state values
             newState.push(todo(undefined, action));
@@ -183,89 +44,6 @@ const todos = (state = [], action) => {
       }
 };
 
-const testAddTodo = () => {
-  const stateBefore = [];
-  const action = {
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
-  }
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    }
-  ];
-
-  deepFreeze(stateBefore);
-  deepFreeze(stateAfter);
-
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter);
-};
-
-testAddTodo();
-
-const testToggleTodoOld = () => {
-    const todoBefore = {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    }
-    const todoAfter  = {
-      id: 0,
-      text: 'Learn Redux',
-      completed: true
-    }
-    expect(
-      toggleTodo(todoBefore)
-    ).toEqual(todoAfter);
-  }
-
-testToggleTodoOld();
-
-const testToggleTodo = () => {
-    const stateBefore = [
-    {
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: false
-    }
-    ];
-    const action = {
-      type: 'TOGGLE_TODO',
-      id: 1
-    };
-    const stateAfter = [
-    {
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: true
-    }
-    ];
-  
-    deepFreeze(stateBefore);
-    deepFreeze(action);
-  
-    expect(
-      todos(stateBefore, action)
-    ).toEqual(stateAfter);
-  }
-
-
-testToggleTodo(); // run the test
 
 const visibilityFilter = (
     state = 'SHOW_ALL', // default state
@@ -280,31 +58,11 @@ const visibilityFilter = (
 };
 
 
-// const combineReducers = (reducers) => {
-//     return (state = {}, action) => {
-//       return Object.keys(reducers).reduce(
-//         (nextState, key) => {
-//           nextState[key] = reducers[key](
-//             state[key],
-//             action
-//           );
-//           return nextState;
-//         },
-//         {} // empty initial nextState
-//       );
-//     };
-//   };
-
 const todoApp = combineReducers({
     todos,
     visibilityFilter
   });
 
-// console.log('Dispatching SET_VISIBILITY_FILTER');
-// store.dispatch({
-//   type: 'SET_VISIBILITY_FILTER',
-//   filter: 'SHOW_COMPLETED'
-// });
 
 const store = createStore(todoApp,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
@@ -528,7 +286,7 @@ const TodoApp = ({
   );
 
 
-store.subscribe(render);
-render();
+//store.subscribe(render);
+//render();
 console.log('All tests are passed');
 
